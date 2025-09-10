6e92ee6f89cf9222b85a2b1d1b3111244ad2d81f
alark <gitartem97@gmail.com>
2025-09-10 16:50:47 +0300

    Алгоритм Дейкстры

--- grokking_algoritms.py
+++ grokking_algoritms.py
@@ -1,34 +1,57 @@
-from collections import deque
-def is_seller(name):
-    """Проверка, продаёт ли человек манго. Если имя заканчивается
-    на "m", то человек - продавец манго"""
-    return name[-1] == 'm'
+#Алгоритм Дейкстры
+#создаём хеш-таблицу, содержащую все узлы и сумму ребер
+graph = {'start': {'a': 10,},
+         'a': {'b': 20,},
+         'b': {'c': 1,
+               'finish': 30},
+         'c': {'a': 1,},
+         'finish':{},
+}
 
-def search(name):
-    """Функция поиска в ширину на примере поиска человека,
-    который продаёт манго"""
-    search_queue = deque()
-    search_queue += graph[name]
-    searched = []
-    while search_queue:
-        person = search_queue.popleft()
-        if person not in searched:
-            if is_seller(person):
-                print(person + " is a mango seller!")
-                return True
-            else:
-                search_queue += graph[person]
-                searched.append(person)
-    return False
+#хеш-таблица стоимостей, которая будет изменяться во время 
+# выполнения алгоритма
+infinity = float('inf')
+costs = {'a': 10,
+         'b': 30,
+         'c': 31,
+         'finish': infinity}
 
-graph = {}
-graph['you'] = ['alice', 'bob', 'claire']
-graph['bob'] = ['anuj', 'peggy']
-graph['alice'] = ['peggy']
-graph['claire'] = ['thom', 'jonny']
-graph['anuj'] = []
-graph['peggy'] = []
-graph['thom'] = []
-graph['johny'] = []
+#хеш-таблица для указания родителей узлов
+parents ={'a': 'b',
+          'b': 'a',
+          'c': 'b',
+          'finish': 'b',
+          }
 
-search('you')
+#список для отслеживания всех уже обработанных узлов
+proccessed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in proccessed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    proccessed.append(node)
+    node = find_lowest_cost_node(costs)
+
+print(costs)
+print(costs['finish'])
+print(parents)
+
+#8
+#60
+#4
